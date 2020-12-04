4319ae2d83908c9ce95bf768891dbe0dbd53e109
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-12-04 10:28:17 +1000

    update

--- src/isEven.spec.js
+++ src/isEven.spec.js
@@ -1,7 +1,19 @@
 function isEven(x) {
-  return false
+  return x % 2 === 0 
 }
 
-test('isEven', () => {
-  expect(isEven(1)).toBe(false)
+// describe, it
+describe('isEven', () => {
+
+  it('returns true for an odd value', () => {
+    expect(isEven(0)).toBe(true)
+  })
+
+  it('returns false for an odd value', () => {
+    expect(isEven(1)).toBe(false)
+  })
+
+  it('returns true for an even value', () => {
+    expect(isEven(2)).toBe(true)
+  })
 })

6058421c3fc5f9c6e809a13d84ceaa0289d260ca
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-02-20 17:35:30 +1000

    update

--- src/matchers.spec.js
+++ src/matchers.spec.js
@@ -5,7 +5,15 @@ test('', () => {
     }
   }
 
-  expect(a).toHaveProperty(
-    'foo', { num : 1 }
-  )
+  const p = new Promise(res => {
+    setTimeout(() => {
+      res(a)
+    }, 1000)
+  })
+
+  return expect(p).resolves.toEqual({
+    foo: {
+      num: 1
+    }
+  })
 })

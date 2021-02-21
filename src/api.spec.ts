11cbe3d5b96a872df27972b59f6704ae184881ab
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-02-21 15:44:03 +1000

    wip

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,2 +1,17 @@
+import * as express from 'express'
+import * as axios from 'axios'
+
+const createApp = () => {
+  const app = express()
+  app.get('/data', (req, res) => {
+    res.json({ foo: 'bar' })
+  })
+  return app
+}
+
 test('', () => {
+  const app = createApp()
+  app.listen(8080)
+  axios.default.get('http://localhost:8080/data')
+    .then(res => console.log(res.data))
 })
